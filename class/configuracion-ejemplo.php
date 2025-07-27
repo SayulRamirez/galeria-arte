2684373d708ea4345a52591f239b1e01b4498c4b
SayulRamirez <sayulramirez@gmail.com>
2025-07-27 10:58:00 -0600

    se agrego el archivo de ejemplo para la configuracion

---
+++ class/configuracion-ejemplo.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_HOST_EJEMPLO', 'Tu host');
+define('DB_NAME_EJEMPLO', 'Tu base de datos');
+define('DB_USER_EJEMPLO', 'Tu usuario');
+define('DB_PASS_EJEMPLO', 'Tu contraseña');
+?>
