e72ef85f4d8305de75df4ae6a451126606a2db3a
SayulRamirez <sayulramirez@gmail.com>
2025-08-01 18:55:27 -0600

    feature: se agrego la pagina principal

--- index.php
+++ index.php
@@ -1,80 +1,63 @@
-<?php 
-include_once('class/Conexion.php');
-
-$conexion = new Conexion();
-
-$query = "SELECT * FROM obras";
-
-$resultados = $conexion->select($query, []);
-?>
-
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ver obras</title>
-    <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-            background-color: #F8F8F8;
-            margin: 20px;
-        }
-    
-        h1 {
-            text-align: center;
-        }
-    
-        .galeria {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            gap: 20px;
-        }
-    
-        .obra {
-            background-color: white;
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgb(0, 0, 0, 0.1);
-            width: 30px;
-            padding: 15px;
-        }
-    
-        .obra img {
-            width: 100%;
-            height: auto;
-            border-radius: 5px;
-        }
-    
-        .obra h3, .obra p {
-            margin: 10px 0;
-        }
-    </style>
+    <title>The Metal Galery</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
+
 <body>
-    <h1>Obras registradas</h1>
-    <div class="galeria">
-        <?php if (count($resultados) > 0) {
-            foreach($resultados as $result) {
-                $autor = $result['autor'];
-                $titulo = $result['titulo'];
-                $imagen = $result['imagen_path'];
-                $descripcion = $result['descripcion'];
-        ?>
-        <div class="obra">
-            <img src="<?php echo $imagen; ?>" alt="Obra">
-            <h3><?php echo htmlspecialchars($titulo); ?></h3>
-            <p><strong>Autor:</strong><?php echo htmlspecialchars($autor); ?></p>
-            <p><?php echo htmlspecialchars($descripcion); ?></p>
+    <div class="container-xl mt-3">
+        <h1 class="text-center mt-5 mb-5">The Metal Galery</h1>
+        <div id="container" class="row row-cols-1 row-cols-sm-2">
         </div>
-        <?php
-            }
-        } else {
-        ?>
-        <p>No hay obras registradas</p>
-        <?php
-        }
-        ?>
     </div>
+    <template id="cardObra">
+        <div class="col">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="" class="img-fluid rounded-start" alt="">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title"></h5>
+                            <p class="card-text"></p>
+                            <p class="card-autor"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </template>
+    <script>
+        const container = document.getElementById('container');
+        const template = document.getElementById('cardObra');
+
+        fetch('class/GaleriaController.php', { method: 'GET' })
+        .then(res => res.json())
+        .then(obras => {
+
+            obras.forEach(obra => {
+                const clone = template.content.cloneNode(true);
+
+                clone.querySelector('img').src = obra.imagen_path || 'placeholder.jpg';
+                // clone.querySelector('img').alt = obra.titulo;
+                clone.querySelector('.card-title').textContent = obra.titulo;
+                clone.querySelector('.card-text').textContent = obra.descripcion;
+                clone.querySelector('.card-autor').textContent = obra.autor;
+
+                container.appendChild(clone);
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            console.error('Ocurrió un error');
+        });
+    </script>
 </body>
+
 </html>
